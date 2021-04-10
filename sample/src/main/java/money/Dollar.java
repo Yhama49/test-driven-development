a3541c5134675a4cb0409b462e3bd2b76f50f3c3
濱岡佑介 <t.k.s.d.0409@gmail.com>
2021-04-10 10:15:57 +0900

    第8章までのコード

---
+++ sample/src/main/java/money/Dollar.java
@@ -0,0 +1,9 @@
+package money;
+class Dollar extends Money{
+    Dollar(int amount){
+        this.amount = amount;
+    }
+    Money times(int multiplier){
+        return new Dollar(amount * multiplier);
+    }
+}
