a80fd41e06cbdf5ac9c0b45fea71795063196b89
濱岡佑介 <t.k.s.d.0409@gmail.com>
2021-04-10 18:03:46 +0900

    第11章までのコード

--- sample/src/main/java/money/Dollar.java
+++
@@ -1,9 +0,0 @@
-package money;
-class Dollar extends Money{
-    Dollar(int amount){
-        this.amount = amount;
-    }
-    Money times(int multiplier){
-        return new Dollar(amount * multiplier);
-    }
-}
--- sample/src/main/java/money/Money.java
+++ sample/src/main/java/money/Money.java
@@ -1,20 +1,33 @@
 package money;
 
-abstract class Money {
+class Money {
     protected int amount;
-    abstract Money times(int multiplier);
+    protected String currency;
+    Money(int amount, String currency){
+        this.amount = amount;
+        this.currency = currency;
+    }
+    Money times(int multiplier){
+        return new Money(amount * multiplier, currency);
+    }
+
+    String currency(){
+        return currency;
+    }
 
     public boolean equals(Object object){
         Money money = (Money) object;
         return amount == money.amount
-        && getClass().equals(money.getClass());
+        && currency().equals(money.currency());
+    }
+    public String toString(){
+        return amount + " " + currency;
     }
     static Money dollar(int amount){
-        return new Dollar(amount);
+        return new Money(amount, "USD");
     }
-
     static Money franc(int amount){
-        return new Franc(amount);
+        return new Money(amount, "CHF");
     }
     
 }
